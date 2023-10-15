804200687c5d45358d30a448fd35f3e5bd89c314
SMarkus27 <pereira.markus.27@hotmail.com>
2023-10-15 11:43:15 -0300

    chore: created mongodb infrastructure

---
+++ src/infrastructures/mongodb/infrastructure.ts
@@ -0,0 +1,26 @@
+import { connect } from "mongoose";
+import * as mongoose from "mongoose";
+import { config } from "dotenv";
+import * as process from "process";
+import {IMongoDBInfrastructure} from "../core/interfaces/infrastructures/mongodb/interface";
+config()
+
+export class MongoDBInfrastructure implements IMongoDBInfrastructure {
+
+    async getClient(): Promise<mongoose.Mongoose>{
+        try {
+            console.log("Connecting to MongoDB")
+            const mongoClient = await connect(
+                process.env.MONGODB_CONNECTION_STRING,
+                {
+                    dbName: process.env.MONGODB_DATABASE_NAME,
+                })
+
+            return mongoClient
+        }
+        catch (error) {
+            console.log("Something happening")
+
+        }
+    }
+}
