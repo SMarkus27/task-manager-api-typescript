ba2a3437670191d80758149437079b5c2bf5a74c
SMarkus27 <pereira.markus.27@hotmail.com>
2023-11-02 12:09:36 -0300

    test: users repository tests

---
+++ tests/repositories/users/find_user.spec.ts
@@ -0,0 +1,27 @@
+import {UsersRepository} from "../../../src/repositories/users/repository";
+import {UserModel} from "../../../src/domain/models/users/model";
+
+jest.mock("../../../src/domain/models/users/model")
+
+describe("Users Repository", () => {
+    let usersRepository: UsersRepository;
+
+    beforeEach(() => {
+        usersRepository = new UsersRepository();
+    });
+
+
+    it('should return user', async () => {
+        const userData = {
+            username: "xaps",
+            password: "1234567",
+            email: "test2@gmail.com"
+        };
+
+        await usersRepository.findUser(userData, {}, {})
+
+        expect(UserModel.findOne).toBeCalledWith(userData, {}, {})
+    });
+
+
+});
